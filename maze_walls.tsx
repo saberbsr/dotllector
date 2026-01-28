55719096eda14c34502f757eb892b71137a31a09
saberbsr <timurchikoff@yandex.ru>
2026-01-28 18:10:12 +0300

    dotllector

--- maze_walls.tsx
+++ maze_walls.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="maze_walls" tilewidth="16" tileheight="16" tilecount="28" columns="7">
- <image source="images/dotllecter_map_tiles.png" width="112" height="64"/>
+ <image source="images/dotllector_map_tiles.png" width="112" height="64"/>
 </tileset>
--- tiles_extra.tsx
+++ tiles_extra.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="tiles_extra" tilewidth="16" tileheight="16" tilecount="28" columns="7">
- <image source="images/dotllecter_extra_tiles.png" width="112" height="64"/>
+ <image source="images/dotllector_extra_tiles.png" width="112" height="64"/>
 </tileset>
